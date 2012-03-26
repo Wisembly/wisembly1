e2be04317897fe43a206f84507a3d32a4d5246ac
Maël Nison <nison.mael@gmail.com>
2012-03-26 18:47:17 +0200

    Adds an index file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,6 +9,8 @@ $app = new Silex\Application();
 $app->register(new \Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__ . '/../src/Resources/views'));
 $app->register(new \Silex\Provider\DoctrineServiceProvider(), array('db.options' => $database, 'db.dbal.class_path' => __DIR__ . '/../vendor/Doctrine/dbal/lib', 'db.common.class_path' => __DIR__ . '/../vendor/Doctrine/common/lib'));
 
+$app->register(new SilexCMS\Page\StaticPage('/', 'index.html.twig'));
+
 $app['debug'] = true;
 $app['twig']->addExtension(new Twig_Extension_Debug());
 $app['twig']->enableDebug();
