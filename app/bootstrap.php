53a29185867211986aaec9083d3564170eec8fdd
Maël Nison <nison.mael@gmail.com>
2012-03-27 11:01:59 +0200

    Adds a DataSet example

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,6 +9,7 @@ $app = new Silex\Application();
 $app->register(new \Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__ . '/../src/Resources/views'));
 $app->register(new \Silex\Provider\DoctrineServiceProvider(), array('db.options' => $database, 'db.dbal.class_path' => __DIR__ . '/../vendor/Doctrine/dbal/lib', 'db.common.class_path' => __DIR__ . '/../vendor/Doctrine/common/lib'));
 
+$app->register(new SilexCMS\Set\DataSet('foobars'));
 $app->register(new SilexCMS\Page\StaticPage('/', 'index.html.twig'));
 
 $app['debug'] = true;
