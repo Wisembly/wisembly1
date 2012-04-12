cec4c13661ce6288bd1bf191ee61a18f0d2f5e67
Nicolas Chenet <nicolas@balloonup.com>
2012-04-12 12:33:20 +0200

    first commit balloonup project

--- app/startup.php
+++ app/startup.php
@@ -2,7 +2,7 @@
 
 use Silex\Application;
 
-$app->register(new SilexCMS\Set\DataSet('foobars'));
+// $app->register(new SilexCMS\Set\DataSet('foobars'));
 $app->register(new SilexCMS\Page\StaticPage('/', 'index.html.twig'));
 $app->register(new SilexCMS\Page\StaticPage('/contact', 'contact.html.twig'));
 
