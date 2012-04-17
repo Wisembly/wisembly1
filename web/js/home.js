419f7c8fd3791df48b61e5b4465a6e2c836d4480
Nicolas Chenet <nicolas@balloonup.com>
2012-04-17 17:50:16 +0200

    Home and generic page templates

--- web/js/home.js
+++ web/js/home.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($) {
 
+    // Navigation
+    $('.nav li:eq(0)').addClass('active');
+
     var slider = $("#slider-wrapper")
         .carousel({
             interval: 4000
@@ -15,8 +18,24 @@ jQuery(document).ready(function($) {
         e.preventDefault();
     });
 
-    $("#customer-quotes-wrapper").carousel({
-        interval: 10000
-    })
+
+    $('.slider-features') .click(function(){
+        var link = $(this).find('a').attr('href');
+        document.location.href = link;
+    });
+
+
+    var slider_customers = $("#customer-quotes-wrapper")
+        .carousel()
+        .bind('slid', function(){
+            var index = $(this).find(".active").index();
+            $(".customers").find("a.customer").removeClass('active').eq(index).addClass('active');
+        });
+
+    $('.customers a.customer').click(function(e){
+        var index= $(this).index();
+        slider_customers.carousel(index-1);
+        e.preventDefault();
+    });
 
 });
