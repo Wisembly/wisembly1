341865eccdc82d3951d1d152c0f9720c35a07445
Nicolas Chenet <nicolas@balloonup.com>
2012-05-14 14:28:30 +0200

    Cases: using SilexCMS

--- app/startup.php
+++ app/startup.php
@@ -2,7 +2,12 @@
 
 use Silex\Application;
 
-// $app->register(new SilexCMS\Set\DataSet('foobars'));
+// Datasets
+$app->register(new SilexCMS\Set\DataSet('cases_featured', 'cases'));
+$app->register(new SilexCMS\Set\DataSet('cases'));
+$app->register(new SilexCMS\Set\DataSet('clients'));
+
+// Pages
 $app->register(new SilexCMS\Page\StaticPage('/',            'index.html.twig'       ));
 $app->register(new SilexCMS\Page\StaticPage('/features',    'features.html.twig'    ));
 $app->register(new SilexCMS\Page\StaticPage('/cases',       'cases.html.twig'       ));
