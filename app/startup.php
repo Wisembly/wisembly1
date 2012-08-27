c60cf45155bf27e14bbbbf16a534d2fcfd9958a3
Nicolas Chenet <nicolas@balloonup.com>
2012-08-27 10:44:48 +0200

    Modified datasets

--- app/startup.php
+++ app/startup.php
@@ -10,12 +10,14 @@ $app->register(new SilexCMS\Set\DataSet('clients', 'clients'));
 $app->register(new SilexCMS\Set\DataSet('family', 'family'));
 $app->register(new SilexCMS\Set\DataSet('filters', 'filters'));
 $app->register(new SilexCMS\Set\DataSet('features', 'features'));
+$app->register(new SilexCMS\Set\DataSet('history', 'history'));
 
 // Pages
 $app->register(new SilexCMS\Page\StaticPage('index', 	'/',            'index.html.twig'       ));
 $app->register(new SilexCMS\Page\StaticPage('cases', 	'/cases',       'cases.html.twig'       ));
 $app->register(new SilexCMS\Page\StaticPage('plans', 	'/plans',       'plans.html.twig'       ));
 $app->register(new SilexCMS\Page\StaticPage('contact', 	'/contact',     'contact.html.twig'     ));
+$app->register(new SilexCMS\Page\StaticPage('about', 	'/about',     	'about.html.twig'     	));
 
 $app->register(new SilexCMS\Page\DynamicPage('features', '/features/{slug}', 'features.html.twig', 'filters'  ));
 $app->register(new SilexCMS\Page\DynamicPage('cases_categories', '/cases/{slug}', 'cases_categories.html.twig', 'cases_categories'  ));
