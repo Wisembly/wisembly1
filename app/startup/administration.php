56596b63cf362484357c325b4739de9ebebd85c8
Guillaume Potier <cobalt2760@gmail.com>
2012-08-28 17:52:50 +0200

    - removed remanent administration all rows

--- app/startup/administration.php
+++ app/startup/administration.php
@@ -29,34 +29,6 @@ $app->match('/administration/{table}', function (Application $app, Request $req,
 })
 ->bind('administration');
 
-$app->match('/administration/{table}/all', function (Application $app, Request $req, $table) {
-
-    if (is_null($app['security']->getUsername())) {
-        return $app->redirect($app['url_generator']->generate('index'));
-    }
-
-    $app['db']->query("SET NAMES 'UTF8'");
-    $rows = array('rows' => $app['db']->fetchAll("SELECT * FROM `{$table}`"));
-    $form = $app['form.factory']->create(new TableType($app, $table), $rows);
-
-    if ($req->getMethod() === 'POST') {
-        $form->bindRequest($req);
-
-        if ($form->isValid()) {
-            $data = $form->getData();
-
-            foreach ($data['rows'] as $row) {
-                $where = array('id' => $row['id']);
-                unset($row['id']);
-                $app['db']->update($table, $row, $where);
-            }
-        }
-    }
-
-    return new TransientResponse($app['twig'], 'administration_all.html.twig', array('table' => $table, 'form' => $form->createView()));
-})
-->bind('administration_all');
-
 $app->match('/administration/{table}/{id}', function (Application $app, Request $req, $table, $id) {
 
     if (is_null($app['security']->getUsername())) {
