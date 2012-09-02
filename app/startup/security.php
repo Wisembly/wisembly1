4ea8978771ce1673970eb5509403ae18ef926a19
Guillaume Potier <cobalt2760@gmail.com>
2012-09-02 15:06:04 +0200

    - tweaked a bit login / logout actions

--- app/startup/security.php
+++ app/startup/security.php
@@ -3,35 +3,31 @@
 use Silex\Application;
 
 use Symfony\Component\HttpFoundation\Request;
+use SilexCMS\Response\TransientResponse;
 
 $app->register(new SilexCMS\Security\Firewall('security', require __DIR__ . '/../config/users.php'));
 
-$app->register(new SilexCMS\Page\StaticPage('login_success', '/login/success', 'security/login/success.html.twig'));
-$app->register(new SilexCMS\Page\StaticPage('login_failure', '/login/failure', 'security/login/failure.html.twig'));
-
-$app->register(new SilexCMS\Page\StaticPage('logout_success', '/logout/success', 'security/logout/success.html.twig'));
-$app->register(new SilexCMS\Page\StaticPage('logout_failure', '/logout/failure', 'security/logout/failure.html.twig'));
-
-$app->register(new SilexCMS\Page\StaticPage('login', '/login', 'security/login.html.twig'));
-$app->register(new SilexCMS\Page\StaticPage('logout', '/', 'index.html.twig'));
-
-$app->post('/login', function (Application $app, Request $req) {
+$app->match('/login', function (Application $app, Request $req) {
     $security = $app['security'];
-    
-    if ($security->bindSession()->getUserName() || $security->bindRequest($req)->getUsername()) {
+
+    if (null !== $security->getUserName()) {
         return $app->redirect($app['url_generator']->generate('administration_hub'));
-    } else {
-        return $app->redirect($app['url_generator']->generate('login_failure'));
     }
-});
 
-$app->post('/logout', function (Application $app, Request $req) {
-    $security = $app['security'];
-
-    if ($security->isLogged()) {
-        $security->unbind();
-        return $app->redirect($app['url_generator']->generate('index'));
-    } else {
-        return $app->redirect($app['url_generator']->generate('logout_failure'));
+    if ($req->getMethod() === 'POST') {
+         if ($security->bindSession()->getUserName() || $security->bindRequest($req)->getUsername()) {
+             return $app->redirect($app['url_generator']->generate('administration_hub'));
+         } else {
+            $error = "L'identification a échouée.";
+        }
     }
-});
+
+    return new TransientResponse($app['twig'], 'security/login.html.twig', array('error' => isset($error) ? $error : null));
+})
+->bind('login');
+
+$app->get('/logout', function (Application $app, Request $req) {
+    $app['security']->unbind();
+    return $app->redirect($app['url_generator']->generate('index'));
+})
+->bind('logout');
