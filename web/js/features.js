eccbabe0a046795d73c06756945e4567bfbe719a
Nicolas Chenet <nicolas@balloonup.com>
2012-09-04 11:38:50 +0200

    fixed movingGrid bug / various fixes

--- web/js/features.js
+++ web/js/features.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
     // Navigation
-    $('.nav li:eq(1)').addClass('active');
+    $('.nav li:eq(0)').addClass('active');
 
    $('[rel="tooltip"]').tooltip();
 });
