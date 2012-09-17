cc3a018102da0d76a93ec1ff74c00928a688d360
Guillaume Potier <cobalt2760@gmail.com>
2012-09-17 16:12:33 +0200

    - updated .htacess in web to redirect seamlessly to /fr or /en

--- web/index.php
+++ web/index.php
@@ -1,4 +1,40 @@
 <?php
 
-header('Location: fr');
+function url_exists($url) {
+    $ch = @curl_init($url);
+    @curl_setopt($ch, CURLOPT_HEADER, TRUE);
+    @curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+    @curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+    @curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $status = array();
+    preg_match('/HTTP\/.* ([0-9]+) .*/', @curl_exec($ch) , $status);
+
+    return isset($status[1]) && $status[1] == 200;
+}
+
+function url_redirect($url, $header = true)
+{
+    if ($header) {
+        header('HTTP/1.1 301 Moved Permanently', false, 301);
+    }
+
+    header("Location: {$url}");
+    die();
+}
+
+$url = $_SERVER["REQUEST_URI"];
+$host = $_SERVER["HTTP_HOST"];
+
+if ('/' === $url) {
+    url_redirect('fr');
+} else {
+    if (url_exists($host . '/fr' . $url)) {
+        url_redirect('http://' . $host . '/fr' . $url);
+    } elseif (url_exists($host . '/en' . $url)) {
+        url_redirect('http://' . $host . '/en' . $url);
+    }
+
+    url_redirect('404.html', false);
+}
+
 die();
