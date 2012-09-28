979cb1ec28b645ab425b5acfd86fafdfc3f549fa
Nicolas Chenet <nicolas@balloonup.com>
2012-09-28 12:01:26 +0200

    Ported pricings - step 2

--- web/js/plans.js
+++ web/js/plans.js
@@ -1,13 +1,36 @@
-jQuery(document).ready(function($) {
+(function (plan, translator, router) {
 
-    // Navigation
-    $('.nav li:eq(3)').addClass('active');
+    $(function () {
+
+    	// Navigation
+    	$('.nav li:eq(3)').addClass('active');
 
 
-     // Tooltips
-    $('body').tooltip({
-        // On utilise 'selector' pour utiliser la délégation d'évènement (équivalent de 'live')
-        selector: '[rel=tooltip], .tooltip'
+     	// Tooltips
+	    $('body').tooltip({
+	        // On utilise 'selector' pour utiliser la délégation d'évènement (équivalent de 'live')
+	        selector: '[rel=tooltip], .tooltip'
+	    });
+
+	    // Let's attach our tools :)
+	    plan.attachTranslator(translator);
+	    plan.attachRouter(router);
+
+        plan.expected_attendees
+            .on("keyup", function (event) {
+                plan
+                    .clearErrors()
+                    .updateHref()
+                    .updatePrice(event.currentTarget);
+            })
+            .focus();
+
+        plan.target_btn.on("click", function () {
+            return !(plan.expected_attendees.val() == '');
+        });
+
     });
 
-});
+})(new Plan(), new Translator(lang), new Router(lang));
+
+
