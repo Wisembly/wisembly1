0f06b6c9410cfaa2bc889936a6d64589533b6396
Guillaume Potier <cobalt2760@gmail.com>
2013-01-13 13:01:10 +0100

    - added multi flag support. to be improved

--- web/index.php
+++ web/index.php
@@ -24,17 +24,38 @@ function url_redirect($url, $header = true)
 
 $url = $_SERVER["REQUEST_URI"];
 $host = $_SERVER["HTTP_HOST"];
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
+// redirect here if we have a known domain
+if (false !== strpos($host, '.fr') || false !== strpos($host, '.ch') && url_exists("http://wisembly.com/fr{$url}")) {
+    url_redirect("http://wisembly.com/fr{$url}"); 
+} elseif (false !== strpos($host, '.es') && url_exists("http://wisembly.com/es{$url}")) {
+    url_redirect("http://wisembly.com/es{$url}"); 
+} elseif (false !== strpos($host, '.de') && url_exists("http://wisembly.com/de{$url}")) {
+    url_redirect("http://wisembly.com/de{$url}"); 
+}
+
+// redirect here depending on user browser language if on wisembly.com/
 if ('/' === $url) {
-    url_redirect('fr');
-} else {
-    if (url_exists($host . '/fr' . $url)) {
-        url_redirect('http://' . $host . '/fr' . $url);
-    } elseif (url_exists($host . '/en' . $url)) {
-        url_redirect('http://' . $host . '/en' . $url);
+    if (url_exists("http://{$lang}{$url}")) {
+        url_redirect("http://{$host}/{$lang}{$url}");
     }
 
-    url_redirect('404.html', false);
+url_redirect("http://wisembly.com/en/");
 }
+
+// redirect here if direct wisembly full url on .com
+if (url_exists($host . '/fr' . $url)) {
+    url_redirect('http://' . $host . '/fr' . $url);
+} elseif (url_exists($host . '/en' . $url)) {
+    url_redirect('http://' . $host . '/en' . $url);
+} elseif (url_exists($host . '/es' . $url)) {
+  url_redirect('http://' . $host . '/es' . $url);
+} elseif (url_exists($host . '/de' . $url)) {
+  url_redirect('http://' . $host . '/de' . $url);
+}
+
+// after all that, did not find your page?? DAMN!
+url_redirect('404.html', false);
 
 die();
